8ecb12ef4e3927c82743c8a3e2621e80aa25385f
Yaroslav Halchenko <debian@onerussian.com>
2023-02-21 11:19:35 -0500

    There is no test_test any longer

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -15,7 +15,6 @@ git-annex-remote-datalad --help
 if [ "$(uname)" = "Linux" ] || hash git-annex; then
     # workaround https://github.com/datalad/datalad/issues/6623
     export PYTHON_KEYRING_BACKEND=keyrings.alt.file.PlaintextKeyring
-    # test_test would be removed as 0.17.1
     # test_subprocess_return_code_capture - https://github.com/datalad/datalad/issues/6109
-    python -m pytest -s -v -k 'not test_test and not test_subprocess_return_code_capture' --pyargs datalad
+    python -m pytest -s -v -k 'not test_subprocess_return_code_capture' --pyargs datalad
 fi
